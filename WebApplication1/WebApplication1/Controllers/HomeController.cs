cce1dd1e786554112504f01b43b029327febaea9
kvbragt@sogyo.nl <kvbragt@sogyo.nl>
2019-01-28 12:10:56 +0100

    initial commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: web
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        protected string playMancala(object sender, EventArgs e)
+        {
+            return "you requested to play mancala";
+        }
+
+    }
+
+}
